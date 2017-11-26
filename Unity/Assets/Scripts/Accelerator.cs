61e267d2bcd0ce9978088e30c3d6ff0a79f15e78
GeibTobias <geib02@gw.uni-passau.de>
2017-11-26 18:36:58 +0100

    merge

--- Unity/Assets/Scripts/Accelerator.cs
+++ Unity/Assets/Scripts/Accelerator.cs
@@ -10,11 +10,15 @@ public class Accelerator : MonoBehaviour {
 	public float accX;
 	public float accY;
 	public float accZ;
+
+	private float th;
+
 	public RestConsumer restConsumer;
 
 	// Use this for initialization
 	void Start () {
 		restConsumer = GameObject.Find("RestConsumer").GetComponent<RestConsumer>();
+		th = 0.5f;
 	}
 
 
@@ -24,6 +28,14 @@ public class Accelerator : MonoBehaviour {
 		accX = Input.acceleration.x ;
 		accY = Input.acceleration.y ;
 		accZ = Input.acceleration.z ;
+
+		if (accX < th) {
+			accX = 0;
+		}
+
+		if (accY < th) {
+			accY = 0;
+		}
 
 		if (accZ > 0) {
 			restConsumer.deincrementZoom ();
