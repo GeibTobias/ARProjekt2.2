a5d5daf8ac91324218e3d61dc63fcfff6feaa7ab
GeibTobias <geib02@gw.uni-passau.de>
2017-11-26 17:41:34 +0100

    Accelerator Script

---
+++ Unity/Assets/Scripts/Accelerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+public class Accelerator : MonoBehaviour {
+
+
+	public float accX;
+	public float accY;
+	public float accZ;
+	public RestConsumer restConsumer;
+
+	// Use this for initialization
+	void Start () {
+		restConsumer = GameObject.Find("RestConsumer").GetComponent<RestConsumer>();
+	}
+
+
+	void Update () 
+	{
+		
+		accX = Input.acceleration.x ;
+		accY = Input.acceleration.y ;
+		accZ = Input.acceleration.z ;
+
+		if (accZ > 0) {
+			restConsumer.deincrementZoom ();
+		} else {
+			restConsumer.incrementZoom ();
+		}
+			
+		try
+		{
+			StartCoroutine(restConsumer.setMapSettings(accX, accY)); 
+		} catch(Exception e)
+		{
+			onRestError(e); 
+		}
+	}
+
+	private void onRestError(Exception e)
+	{
+		Debug.LogError("Error during server request: " + e.Message); 
+	}
+}
